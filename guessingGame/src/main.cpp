2ac0cb07b779eed5ba44a5977692fb803abecae7
jalesnunes <42074619+jalesnunes@users.noreply.github.com>
2020-07-26 20:34:06 -0600

    Fixing another infinite loop bug and breaking que code into functions

--- guessingGame/src/main.cpp
+++ guessingGame/src/main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <time.h> 
 
 using namespace std;
 
-int main()
+void verifyGuess(float& guess)
 {
-
-    cout << "***************************************" << endl
-         << "* Welcome to guessing game!           *" << endl
-         << "***************************************" << endl;
-
-    const int secretNumber = 42;
-    float guess = 0.0;
-    vector<float> guessCollection;
-
-    cout << "Try to guess the secret number, let's get it start!" << endl;
-
-    cout << "What is your guess, choose a number: ";
-    cin >> guess;
-
     while (cin.fail())
     {
         cout << "ERROR: This is not a valid option." << endl;
@@ -27,6 +14,37 @@ int main()
         cout << "What is your guess, choose a number: ";
         cin >> guess;
     }
+}
+
+float getGuess(float& guess)
+{
+    cout << "What is your guess, choose a number: ";
+    cin >> guess;
+
+    verifyGuess(guess);
+
+    return guess;
+}
+
+int main()
+{
+
+    cout << "***************************************" << endl
+         << "* Welcome to guessing game!           *" << endl
+         << "***************************************" << endl;
+
+    int secretNumber = 0;
+    float guess = 0.0;
+    vector<float> guessCollection;
+    int count = 1;
+    float score = 0;
+
+    srand(time(NULL)); //initialize random seed 
+    secretNumber = rand() % 100;
+
+    cout << "Try to guess the secret number, let's get it start!" << endl;
+
+    getGuess(guess);
 
     do
     {
@@ -40,13 +58,9 @@ int main()
         }
         else if (guess > secretNumber)
         {
-            cout << "Try again, Your guess is bigger than the secret number" ""
+            cout << "Try again, Your guess is bigger than the secret number"
                 << endl;
             guessCollection.push_back(guess);
-        }
-        else
-        {
-            cout << "ERROR: This is not a valid option." << endl;
         }
 
         cout << "\nWrong guesses: ";
@@ -56,12 +70,19 @@ int main()
             cout << guessCollection[i] << " ";
         }
 
-        cout << "\n\nWhat is your guess, choose a number: ";
-        cin >> guess;
+        cout << endl;
+
+        getGuess(guess);
+        count++;
 
     } while (guess != secretNumber);
 
-    cout << "Congratulations you got it!"
+    cout << "Congratulations you got it in "
+        << count
+        << " tries!"
+        << endl
+        << "This is your score: "
+        << score
         << endl;
 
 
